34ea6483509bae08ff4a88622fcedad5f80af60f
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2015-11-20 12:04:04 +0900

    add NLTK pos tag id

--- src/gen_pos_list.py
+++ src/gen_pos_list.py
@@ -17,11 +17,20 @@ import nltk
 import unicodecsv as csv
 from nltk.corpus import webtext
 
+import pos_tag_id
+
 DATA_DIR = "data/"
 
+print("Tokenizing Text...")
 text = nltk.word_tokenize(webtext.raw("firefox.txt"))
+print("Analysing POS tag...")
 pos_list = nltk.pos_tag(text)
 
-with open(DATA_DIR + 'firefox.csv', 'w') as f:
+# Add POS tag ID
+# ("cat", "NN") -> ("cat", "NN", 20)
+pos_list = map(lambda x: (x[0], x[1], pos_tag_id.TAG_IDS[x[1]]), pos_list)
+
+print("Writing to CSV...")
+with open(DATA_DIR + 'firefox-pos-list.csv', 'w') as f:
     writer = csv.writer(f, encoding='utf-8')
     writer.writerows(pos_list)
