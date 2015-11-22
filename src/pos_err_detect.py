82bd45ed45615ca45dbfe049a48cc7320ca5fdb0
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2015-11-22 20:19:26 +0900

    add main error detection module

---
+++ src/pos_err_detect.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This script detect English Grammer Error
+#
+
+import nltk
+from nupic.frameworks.opf.modelfactory import ModelFactory
+from nupic.data.inference_shifter import InferenceShifter
+
+
+model = ModelFactory.loadFromCheckpoint("model/")
+model.disableLearning()
+shifter = InferenceShifter()
+
+
+def posErrDetect(target_str):
+    pos_list = nltk.pos_tag(nltk.word_tokenize(target_str))
+    for row in pos_list:
+        model_input = {"token": row[1]}
+        result = shifter.shift(model.run(model_input))
+        print(row, result.inferences["anomalyScore"])
