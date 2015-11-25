1e76a25c1ff60e22767de7a88bce09937cd24af6
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2015-11-25 21:00:12 +0900

    dont use csv file

--- src/pos_learning.py
+++ src/pos_learning.py
@@ -2,34 +2,32 @@
 # -*- coding: utf-8 -*-
 #
 
-import unicodecsv as csv
 from nupic.data.inference_shifter import InferenceShifter
 from nupic.frameworks.opf.modelfactory import ModelFactory
 import pprint
 import os
 
-import model_params.model_params as model_params
-import pos_tags
+from nltk.corpus import brown
 
-DATA_DIR = "data/"
-INPUT_FILE = "news-pos-list.csv"
+import model_params.model_params as model_params
+
 MODEL_DIR = os.getcwd() + "/model"
 
-def addCategoryEncoder(params):
+def addCategoryEncoder(params, categories):
     params["modelParams"]["sensorParams"]["encoders"].update({ 
         "token": {
             "fieldname": u"token",
             "name": u"token",
             "type": "CategoryEncoder",
-            "categoryList": pos_tags.TAG_IDS,
+            "categoryList": categories,
             "w": 23
         }
     })
     return params
 
 
-def createModel(verbosity=False):
-    model = ModelFactory.create(addCategoryEncoder(model_params.MODEL_PARAMS))
+def createModel(verbosity, categories):
+    model = ModelFactory.create(addCategoryEncoder(model_params.MODEL_PARAMS, categories))
     model.enableInference({"predictedField": "token"})
 
     if verbosity:
@@ -38,32 +36,37 @@ def createModel(verbosity=False):
     return model
 
 
+def fetchCorpus():
+    corpus = brown.tagged_sents(categories="news")
+    categories = list(set(map(lambda x:x[1] , brown.tagged_words(categories="news"))))
+
+    print("corpus: ", corpus)
+    print("categories: ", categories)
+    return corpus, categories
+
+
 def main():
-    model = createModel(verbosity = True)
+    corpus, categories = fetchCorpus()
+    model = createModel(True, categories)
     shifter = InferenceShifter()
+    counter = 1
 
-    input_file_name = DATA_DIR + INPUT_FILE
-    output_file_name = DATA_DIR + "out.csv"
-
-    countor = 1
-    with open(input_file_name, 'r') as f1, open(output_file_name, 'w') as f2:
-        reader = csv.reader(f1)
-        writer = csv.writer(f2)
-        for row in reader:
-            model_input = {"token": row[1]}
+    for sentence in corpus:
+        # reset sequence each sentence
+        model.resetSequenceStates() 
+        for word in sentence:
+            model_input = {"token": word[1]}
             result = shifter.shift(model.run(model_input))
 
-            if countor % 100 == 0:
-                print("input line:", countor)
-            if countor % 1000 == 0:
+            if counter % 100 == 0:
+                print("input line:", counter)
+            if counter % 1000 == 0:
                 print("result:", result)
-            if countor % 5000 == 0:
+            if counter % 5000 == 0:
                 print("save model")
                 model.save(MODEL_DIR)
 
-            writer.writerow(row + [result.inferences["anomalyScore"]])
-
-            countor += 1
+            counter += 1
     print("saving model to", MODEL_DIR)
     model.save(MODEL_DIR)
 
